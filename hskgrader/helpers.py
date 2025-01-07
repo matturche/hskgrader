80a3d02e52dcfe4a1a6fcf154ae43b6243bb803c
Mamosa <matth.more@lilo.org>
2025-01-07 12:07:48 +0800

    trying something

--- hskgrader/helpers.py
+++ hskgrader/helpers.py
@@ -45,7 +45,8 @@ def load_github_text_files() -> Dict[str, str]:
 def load_github_text_file(url: str) -> str:
     response = requests.get(url)
     if response.status_code == 200:
-        return StringIO(response.text)
+        return response.text
+        # return StringIO(response.text)
     else:
         st.error("Failed to load data from GitHub.")
         return ""
